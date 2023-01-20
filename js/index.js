48dc4282b3eb8a283ba8695c39dc9769fa3f761f
Carlos Díaz <cdiaz-2018312@kinal.edu.gt>
2023-01-20 10:21:03 -0600

    Primer comit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+function saludar(){
+   let valor1=document.getElementById('valor1').Value
+    let valor2=document.getElementById('valor2').Value
+    let resultado= parseInt(valor1)+ parseInt(valor2)
+    console.log(resultado)
+}
